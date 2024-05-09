3bc74c9326f7c9d3fa44a8c788b27e60473cf438
lidingding <1940864969@qq.com>
2024-05-09 10:55:33 +0800

    feat：git CI

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,33 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-push_addr=git@github.com:fallenaAngel/blogs.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:你的名字/仓库名称.git
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+push_addr=git@github.com:fallenaAngel/fallenaAngel.github.io.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:你的名字/仓库名称.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+push_branch=master # 推送的分支
 
 # 生成静态文件
-npm run build
+npm run build:win
 
 # 进入生成的文件夹
 cd $dist_path
+
+# deploy to github
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:fallenaAngel/blogs.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://fallenaAngel:${GITHUB_TOKEN}@github.com/fallenaAngel/blogs.git
+  git config --global user.name "lidingding"
+  git config --global user.email "1940864969@qq.com"
+fi
 
 git init
 git add -A
